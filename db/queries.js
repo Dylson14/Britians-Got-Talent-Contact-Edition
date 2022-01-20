4194325c92954495a83d4b20b195cf3c872b52a7
Dylson Oliveira <dylsonuni@gmail.com>
2022-01-20 21:49:59 +0000

    got the search functionality working

--- db/queries.js
+++ db/queries.js
@@ -1,10 +1,19 @@
 const db = require("../connection");
+const menuPrompt = require("../Prompts/menuPrompt")
 
 const showData = () => {
         db.query("SELECT * from employee", function (err, results){
-        console.table(results)
+        console.table(results);
     })
 }
 
+const showLocation = (answers) => {
+        // console.log("We are in showLocation " + answers.locationInput);
+        db.query(`SELECT * FROM employee WHERE location='${answers.locationInput}'`, function (err, results){
+            console.table(results);
+        })
+      
+}
 
-module.exports = showData;
+
+module.exports = {showData, showLocation};
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const inquirer = require("inquirer");
 const menuPrompt = require("./Prompts/menuPrompt");
 const connectDB = require("./connection");
-const showData = require("./db/queries");
+const {showData, showLocation} = require("./db/queries")
 
 const Start = () => {
     inquirer.prompt(menuPrompt)
     .then((answers) => {
         console.log(answers);
         showData();
+        showLocation(answers);
     })
 }
 
