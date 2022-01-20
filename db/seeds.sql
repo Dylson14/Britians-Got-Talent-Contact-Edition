4f8127120adbdac57639da9b2e3eac49c8f01d5b
Dylson Oliveira <dylsonuni@gmail.com>
2022-01-20 20:25:56 +0000

    seeding the data, providing data to the table

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO employee (name, location, date_of_birth)
+VALUES ("Homer Simpson","Springfield","1956-05-12"),("Frank Reynolds","Philidelphia","1944-11-17"),("Diane Nguyen","Los Angeles","1980-03-19"),("Krusty the Clown","Springfield","1957-10-29");
+
